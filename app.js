d41aa8d2364d15fa66482ba2ef5f0a51f264a68a
bfoley-teamug <withtechron@gmail.com>
2017-09-27 15:01:33 +0000

    corrected

--- app.js
+++ app.js
@@ -1,53 +1,78 @@
-var go = document.getElementById("button"); 
+var min = document.getElementById("min"); 
+var hr = document.getElementById("hr"); 
+var day = document.getElementById("day"); 
+var wk = document.getElementById("wk"); 
+var yr = document.getElementById("yr"); 
+var cent = document.getElementById("cent"); 
 
-go.addEventListener('click', (e) => { 
+
+min.addEventListener('click', (e) => { 
+  const num = document.getElementById("number"); 
+  const input = num.querySelector(".form-control");
+  const answer = input.value;
+  
   e.preventDefault(); 
+  console.log(answer * 60);
+  const p = document.getElementById('display');
+  p.innerHTML = "<h3>That would be a total of <span class='numberColor'>" + answer * 60 + "</span> seconds.</h3>" 
+}); 
+
+
+
+hr.addEventListener('click', (e) => { 
+  const num = document.getElementById("number"); 
+  const input = num.querySelector(".form-control");
+  const answer = input.value;
   
-  const wordA = document.getElementById("wordA"); 
-  const inputA = wordA.querySelector('.inputA');
+  e.preventDefault(); 
+  console.log(answer * 3600);
+  const p = document.getElementById('display');
+  p.innerHTML = "<h3>That would be a total of <span class='numberColor'>" + answer * 3600 + "</span> seconds.</h3>"
+}); 
+
+day.addEventListener('click', (e) => { 
+  const num = document.getElementById("number"); 
+  const input = num.querySelector(".form-control");
+  const answer = input.value;
   
-  const wordB = document.getElementById("wordB"); 
-  const inputB = wordB.querySelector('.inputB');
+  e.preventDefault(); 
+  console.log(answer * 86400);
+  const p = document.getElementById('display');
+  p.innerHTML = "<h3>That would be a total of <span class='numberColor'>" + answer * 86400 + "</span> seconds.</h3>"
+}); 
+
+
+wk.addEventListener('click', (e) => { 
+  const num = document.getElementById("number"); 
+  const input = num.querySelector(".form-control");
+  const answer = input.value;
   
-  const wordC = document.getElementById("wordC"); 
-  const inputC = wordC.querySelector('.inputC');
+  e.preventDefault(); 
+  console.log(answer * 604800);
+  const p = document.getElementById('display');
+  p.innerHTML = "<h3>That would be a total of <span class='numberColor'>" + answer * 604800 + "</span> seconds.</h3>"
+}); 
+
+
+yr.addEventListener('click', (e) => {   
+  const num = document.getElementById("number"); 
+  const input = num.querySelector(".form-control");
+  const answer = input.value;
   
-  const wordD = document.getElementById("wordD"); 
-  const inputD = wordD.querySelector('.inputD');
+  e.preventDefault(); 
+  console.log(answer * 31540000);
+  const p = document.getElementById('display');
+  p.innerHTML = "<h3>That would be a total of <span class='numberColor'>" + answer * 31540000 + "</h3> seconds.</h3>"
+}); 
+
+
+cent.addEventListener('click', (e) => {   
+  const num = document.getElementById("number"); 
+  const input = num.querySelector(".form-control");
+  const answer = input.value;
   
-  const wordE = document.getElementById("wordE"); 
-  const inputE = wordE.querySelector('.inputE');
-  
-  const wordF = document.getElementById("wordF"); 
-  const inputF = wordF.querySelector('.inputF');
-  
-  const wordG = document.getElementById("wordG"); 
-  const inputG = wordG.querySelector('.inputG');
-  
-  const wordH = document.getElementById("wordH"); 
-  const inputH = wordH.querySelector(".inputH");
-  
-  const wordI = document.getElementById("wordI"); 
-  const inputI = wordI.querySelector(".inputI");
-  
-  const wordJ = document.getElementById("wordJ"); 
-  const inputJ = wordJ.querySelector(".inputJ");
-  
-  var result = document.getElementById("result"); 
-  const newWordA = inputA.value;
-  const newWordB = inputB.value;
-  const newWordC = inputC.value;
-  const newWordD = inputD.value;
-  const newWordE = inputE.value;
-  const newWordF = inputF.value;
-  const newWordG = inputG.value;  
-  const newWordH = inputH.value; 
-  const newWordI = inputI.value; 
-  const newWordJ = inputJ.value; 
-  
-  const p = document.getElementById('result');
-  console.log("Today I drove to the " + newWordA);
-  
-  p.textContent = "This morning when I woke up in my " + newWordA + ",  I had a craving for some " + newWordB + ". I decided to find some " + newWordB + ", so I " + newWordC + " on over to the neighbor's house and knocked on their " + newWordD +". They " + newWordE + " invited me inside and gave me some of their leftover " + newWordB +". I asked how their new job was as a " + newWordF + " and they answered it was ' " + newWordG + " ' but their co-workers were " + newWordH + ". I said thanks for the " + newWordB + " and have a great day! As I walked out the " + newWordD + ", I waved and " + newWordI + ". I told myself I will give them some " + newWordJ + " as a token of my appreciation.";               
- }); 
-    
+  e.preventDefault(); 
+  console.log(answer * 3154000000);
+  const p = document.getElementById('display');
+  p.innerHTML = "<h3>That would be a total of <span class='numberColor'>" + answer * 3154000000 + "</h3> seconds.</h3>"
+}); 
